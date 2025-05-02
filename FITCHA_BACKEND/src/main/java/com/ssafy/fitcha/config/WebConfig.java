2ae1ad60b9783117b64ff7b6c33e68d5948f4ce0
oh sewon <tpdnjs7878@naver.com>
2025-05-02 10:18:22 +0900

    Feat: WebConfig에CORS 전역 설정

--- FITCHA_BACKEND/src/main/java/com/ssafy/fitcha/config/WebConfig.java
+++ FITCHA_BACKEND/src/main/java/com/ssafy/fitcha/config/WebConfig.java
@@ -22,5 +22,13 @@ public class WebConfig implements WebMvcConfigurer {
 				.excludePathPatterns("/user/login","/main"); // 제외할 url 지정
 	}
 	
+	// 교차 출처 리소스 공유(CORS) 전역 설정 
+	@Override
+	public void addCorsMappings(CorsRegistry registry) {
+		registry.addMapping("/**")
+				.allowedOrigins("*") // 출처 도메인 (현재는 개발중이라 전체로 설정)-> 추후 나의 프론트엔드 도메인으로 변경 필요
+				.allowedMethods("GET","POST","PUT","DELETE");
+	}
+	
 
 }
