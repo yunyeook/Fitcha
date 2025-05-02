066716bbe5ad8bf027a80a9945b45c377e528742
oh sewon <tpdnjs7878@naver.com>
2025-05-02 10:17:35 +0900

    Feat: 로그인 인터셉터 구현

--- FITCHA_BACKEND/src/main/java/com/ssafy/fitcha/config/WebConfig.java
+++ FITCHA_BACKEND/src/main/java/com/ssafy/fitcha/config/WebConfig.java
@@ -1,5 +1,26 @@
 package com.ssafy.fitcha.config;
 
-public class WebConfig {
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.web.servlet.config.annotation.CorsRegistry;
+import org.springframework.web.servlet.config.annotation.InterceptorRegistry;
+import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
+
+import com.ssafy.fitcha.interceptor.LoginInterceptor;
+
+@Configuration
+public class WebConfig implements WebMvcConfigurer {
+	
+	@Autowired
+	LoginInterceptor loginInterceptor;
+	
+	@Override
+	public void addInterceptors(InterceptorRegistry registry) {
+		
+		registry.addInterceptor(loginInterceptor) // 인터셉터 등록
+				.addPathPatterns("/**") // 매핑할 url 지정
+				.excludePathPatterns("/user/login","/main"); // 제외할 url 지정
+	}
+	
 
 }
