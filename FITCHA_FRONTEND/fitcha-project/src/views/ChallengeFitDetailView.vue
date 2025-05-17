64b09459188ee1fcd7c97c6d5b0628190c9fc2c2
yunyeook <dudhr17050@naver.com>
2025-05-17 20:58:26 +0900

    Feat:challenge Detail

--- FITCHA_FRONTEND/fitcha-project/src/views/ChallengeFitDetailView.vue
+++ FITCHA_FRONTEND/fitcha-project/src/views/ChallengeFitDetailView.vue
@@ -1,16 +1,38 @@
 <template>
   <div>
-    <MainHeader />
-    <MainDetailLayout>
-      <ChallengeFitCardDetail />
-    </MainDetailLayout>
+    <!-- 메인 영역의 헤더 (인사와 검색) -->
+    <header class="main-header"></header>
+
+    <!-- 게시글들 레이아웃을 전체 감싸는 div -->
+    <div class="main-content-wrapper">
+      <div class="main-content-grid">
+        <ChallengeCardDetail :challenge="challenge" />
+      </div>
+    </div>
   </div>
 </template>
 
 <script setup>
-import MainDetailLayout from "@/components/common/MainDetailLayout.vue";
-import ChallengeFitCardDetail from "@/components/challengefit/ChallengeFitCardDetail.vue";
-import MainHeader from "@/components/common/MainHeader.vue";
+import ChallengeCardDetail from '@/components/challengeFit/ChallengeCardDetail.vue';
+import { ref } from 'vue';
+import axios from 'axios';
+import { useRoute, useRouter } from 'vue-router';
+const BASE_URL = 'http://localhost:8080/challenge';
+const route = useRoute();
+const router = useRouter();
+const isViewCounted = ref(route.query.isViewCounted);
+const challengeBoardId = ref(route.params.id);
+const challenge = ref({});
+async function requestChallengeDetail() {
+  const { data } = await axios.get(`${BASE_URL}/${challengeBoardId.value}`, {
+    params: {
+      // user:{}
+      isViewCounted: isViewCounted.value,
+    },
+  });
+  challenge.value = data;
+}
+requestChallengeDetail();
 </script>
 
-<style scoped></style>
+<style lang="scss" scoped></style>
