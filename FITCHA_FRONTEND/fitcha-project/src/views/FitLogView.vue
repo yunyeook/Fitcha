ec43d6479239690d2f0b70757edb2532713cde30
sewon-O <tpdnjs7878@naver.com>
2025-05-26 03:29:02 +0900

    Feat: 인증글 검색 연결

--- FITCHA_FRONTEND/fitcha-project/src/views/FitLogView.vue
+++ FITCHA_FRONTEND/fitcha-project/src/views/FitLogView.vue
@@ -2,27 +2,59 @@
   <div class="page-wrapper">
     <MainHeader />
     <MainContentSearch />
-    <MainGridLayout>
-      <FitLogCard v-for="fitlog in fitlogs" :key="fitlog.proofBoardId" :fitlog="fitlog" />
-    </MainGridLayout>
+    <template v-if="noContent">
+      <MainDetailLayout>
+        <NoContent />
+      </MainDetailLayout>
+    </template>
+    <template v-else>
+      <MainGridLayout>
+        <FitLogCard
+          v-for="fitlog in fitlogs"
+          :key="fitlog.proofBoardId"
+          :fitlog="fitlog"
+        />
+      </MainGridLayout>
+    </template>
   </div>
 </template>
 
 <script setup>
-import MainHeader from '@/components/common/MainHeader.vue';
-import MainContentSearch from '@/components/common/MainContentSearch.vue';
-import MainGridLayout from '@/components/common/MainGridLayout.vue';
-import FitLogCard from '@/components/fitlog/FitLogCard.vue';
-import { ref, onMounted } from 'vue';
-import api from '@/api/api';
+import MainHeader from "@/components/common/MainHeader.vue";
+import MainContentSearch from "@/components/common/MainContentSearch.vue";
+import MainGridLayout from "@/components/common/MainGridLayout.vue";
+import FitLogCard from "@/components/fitlog/FitLogCard.vue";
+import { ref, onMounted, watch } from "vue";
+import { useRoute } from "vue-router";
+import api from "@/api/api";
+import NoContent from "@/components/error/NoContent204.vue";
+import MainDetailLayout from "@/components/common/MainDetailLayout.vue";
 
+const route = useRoute();
 const fitlogs = ref([]);
+const noContent = ref(false);
 
-onMounted(async () => {
-  const { data } = await api.get('/proof');
+async function fetchFitlogs(query = {}) {
+  const params = {};
+  if (query.key && query.word) {
+    params.key = query.key;
+    params.word = query.word;
+  }
+  const { data } = await api.get("/proof", { params });
   fitlogs.value = data;
-  // console.log(fitlogs.value);
+  noContent.value = data.length === 0;
+}
+
+onMounted(() => {
+  fetchFitlogs(route.query);
 });
+
+watch(
+  () => route.query,
+  (newQuery) => {
+    fetchFitlogs(newQuery);
+  }
+);
 </script>
 
 <style scoped>
